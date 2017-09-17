b753a534ee2105171d82c4b2881b739fe7f94fe0
sdsautter <sdsautter@gmail.com>
2017-09-17 19:06:34 -0400

    Added server and route

---
+++ server.js
@@ -0,0 +1,25 @@
+// Declare Dependencies 
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const logger = require('morgan');
+const methodOverride = require('method-override');
+const path = require('path');
+
+// Create Express server
+const app = express();
+const PORT = process.env.PORT || 4012;
+// Define public directory
+app.use(express.static(path.join(__dirname, 'public')));
+// Define BodyParser
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+app.use(bodyParser.json());
+
+//Routes
+require('./routes.js')(app);
+
+app.listen(PORT, () => {
+    console.log(`Localhost:${PORT}`);
+  });
