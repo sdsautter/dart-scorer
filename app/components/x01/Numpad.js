522f7f572af2301a50cb413afe6adb6d3c628f11
sdsautter <sdsautter@gmail.com>
2018-05-06 09:24:32 -0400

    No longer needs to pass through player to methods

--- app/components/x01/Numpad.js
+++ app/components/x01/Numpad.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 export default class Numpad extends Component {
     constructor() {
@@ -20,34 +20,34 @@ export default class Numpad extends Component {
             let originalNumber = this.state.numberEntry;
             let addOnNumber = number;
             let newNumber = `${originalNumber}${addOnNumber}`;
-            this.setState({numberEntry: newNumber});
+            this.setState({ numberEntry: newNumber });
         }
     }
 
     numberRemove() {
         let originalNumber = this.state.numberEntry;
-        let newNumber = originalNumber.slice(0, originalNumber.length-1);
-        this.setState({numberEntry: newNumber});
+        let newNumber = originalNumber.slice(0, originalNumber.length - 1);
+        this.setState({ numberEntry: newNumber });
     }
 
     renderNumberEntry() {
         if (this.state.numberEntry === "") {
             return "0";
         } else {
-        return this.state.numberEntry;
+            return this.state.numberEntry;
         }
     }
 
     scoreEntry() {
         if (parseInt(this.state.numberEntry) <= 180) {
-        this.props.numpadScore(this.props.activeThrower, parseInt(this.state.numberEntry));
-        this.setState({numberEntry: ""});
+            this.props.numpadScore(parseInt(this.state.numberEntry));
+            this.setState({ numberEntry: "" });
         } else if (this.state.numberEntry === "") {
-            this.props.numpadScore(this.props.activeThrower, 0);    
+            this.props.numpadScore(0);
         } else {
-            this.setState({numberEntry: "Too High. Try Again."});
+            this.setState({ numberEntry: "Too High. Try Again." });
             setTimeout(() => {
-                this.setState({numberEntry: ""});
+                this.setState({ numberEntry: "" });
             }, 1500);
         }
     }
@@ -64,43 +64,43 @@ export default class Numpad extends Component {
                     </div>
                     <div className="row">
                         <div className="col-4 number">
-                            <button type="button" className="btn btn-success" onClick={() => {this.numberInput(9)}}>9</button>
+                            <button type="button" className="btn btn-success" onClick={() => { this.numberInput(9) }}>9</button>
                         </div>
                         <div className="col-4 number">
-                            <button type="button" className="btn btn-success" onClick={() => {this.numberInput(8)}}>8</button>
+                            <button type="button" className="btn btn-success" onClick={() => { this.numberInput(8) }}>8</button>
                         </div>
                         <div className="col-4 number">
-                            <button type="button" className="btn btn-success" onClick={() => {this.numberInput(7)}}>7</button>
+                            <button type="button" className="btn btn-success" onClick={() => { this.numberInput(7) }}>7</button>
                         </div>
                         <div className="col-4 number">
-                            <button type="button" className="btn btn-success" onClick={() => {this.numberInput(6)}}>6</button>
+                            <button type="button" className="btn btn-success" onClick={() => { this.numberInput(6) }}>6</button>
                         </div>
                         <div className="col-4 number">
-                            <button type="button" className="btn btn-success" onClick={() => {this.numberInput(5)}}>5</button>
+                            <button type="button" className="btn btn-success" onClick={() => { this.numberInput(5) }}>5</button>
                         </div>
                         <div className="col-4 number">
-                            <button type="button" className="btn btn-success" onClick={() => {this.numberInput(4)}}>4</button>
+                            <button type="button" className="btn btn-success" onClick={() => { this.numberInput(4) }}>4</button>
                         </div>
                         <div className="col-4 number">
-                            <button type="button" className="btn btn-success" onClick={() => {this.numberInput(3)}}>3</button>
+                            <button type="button" className="btn btn-success" onClick={() => { this.numberInput(3) }}>3</button>
                         </div>
                         <div className="col-4 number">
-                            <button type="button" className="btn btn-success" onClick={() => {this.numberInput(2)}}>2</button>
+                            <button type="button" className="btn btn-success" onClick={() => { this.numberInput(2) }}>2</button>
                         </div>
                         <div className="col-4 number">
-                            <button type="button" className="btn btn-success" onClick={() => {this.numberInput(1)}}>1</button>
+                            <button type="button" className="btn btn-success" onClick={() => { this.numberInput(1) }}>1</button>
                         </div>
                         <div className="col-4 number">
-                            <button type="button" className="btn btn-success" onClick={() => {this.numberRemove()}}><img src="./assets/images/left-arrow.png" /></button>
+                            <button type="button" className="btn btn-success" onClick={() => { this.numberRemove() }}><img src="./assets/images/left-arrow.png" /></button>
                         </div>
                         <div className="col-4 number">
-                            <button type="button" className="btn btn-success" onClick={() => {this.numberInput(0)}}>0</button>
+                            <button type="button" className="btn btn-success" onClick={() => { this.numberInput(0) }}>0</button>
                         </div>
                         <div className="col-4 number">
-                            <button type="button" className="btn btn-success" onClick={() => {this.scoreEntry()}}>Enter</button>
+                            <button type="button" className="btn btn-success" onClick={() => { this.scoreEntry() }}>Enter</button>
                         </div>
                         <div className="col undo">
-                            <button type="button" className="btn btn-success" onClick={() => {this.props.numpadUndo()}}>Undo</button>                        
+                            <button type="button" className="btn btn-success" onClick={() => { this.props.numpadUndo() }}>Undo</button>
                         </div>
                     </div>
                 </div>
@@ -113,7 +113,7 @@ export default class Numpad extends Component {
         {
             document.onkeyup = (event) => {
                 var UserInput = event.key;
-                switch(UserInput) {
+                switch (UserInput) {
                     case "9":
                         this.numberInput(9);
                         break;
@@ -147,7 +147,7 @@ export default class Numpad extends Component {
                     case "Enter":
                         this.scoreEntry();
                         break;
-                    case "Backspace": 
+                    case "Backspace":
                         this.numberRemove();
                 }
             }
@@ -156,7 +156,7 @@ export default class Numpad extends Component {
             <div>
                 {this.numpadRender()}
             </div>
-        
+
         )
     }
 }
