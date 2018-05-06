ddf9d67de4e240ce064b136c88588607bc49f4bf
sdsautter <sdsautter@gmail.com>
2018-05-06 10:55:25 -0400

    Adds routes for the games and removes home route

--- routes.js
+++ routes.js
@@ -1,8 +1,13 @@
 const path = require('path');
 
 module.exports = (app) => {
-  app.get('/',
-    (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
-    });
+    app.get('/cricket',
+        (req, res) => {
+            res.sendFile(path.join(__dirname, './public/index.html'));
+        });
+
+    app.get('/x01',
+        (req, res) => {
+            res.sendFile(path.join(__dirname, './public/index.html'));
+        });
 };
